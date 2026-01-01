9eaa0a9c408b2ad5d65269cd05e87deb349779bd
unknown <ayazerguine.dev@gmail.com>
2026-01-01 18:19:22 +0100

    fix headers

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(helmet({
 app.use(cors({
   origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 app.use(express.json());
