65630456b1ba91142ca427355d179d32aacfc38e
unknown <ayazerguine.dev@gmail.com>
2026-01-01 20:52:49 +0100

    allow proxy

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set('trust proxy', 1);
 
 app.use("/", router);
 app.use('/uploads', express.static(join(__dirname, 'uploads')));
