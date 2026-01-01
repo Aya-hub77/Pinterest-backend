9ac5921856eee99627ea83341e9cd34ea197a9ea
unknown <ayazerguine.dev@gmail.com>
2026-01-01 15:12:46 +0100

    Disable Redis for production

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { signup, login, logout, refreshToken } from "../controllers/authController.js";
-import { loginLimiter } from "../middleware/loginLimitter.js";
 import { sanitizeInput } from "../middleware/sanitizeInput.js";
 import { verifyCsrf } from "../middleware/verifyCsrf.js";
 import { signupValidator } from "../middleware/signupValidator.js";
@@ -9,7 +8,7 @@ import { loginValidator } from "../middleware/loginValidator.js";
 const router = Router();
 
 router.post("/signup", verifyCsrf, sanitizeInput(["username", "email", "password"]), signupValidator, signup);
-router.post("/login", verifyCsrf, loginLimiter, sanitizeInput(["email", "password"]), loginValidator, login);
+router.post("/login", verifyCsrf, sanitizeInput(["email", "password"]), loginValidator, login);
 router.post("/logout", logout);
 router.get("/refresh-token", refreshToken);
 
